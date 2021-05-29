3efd8d971483f9e5dd3e9a23472406a4f94e4660
Batu <batuaytemiz@gmail.com>
2021-05-28 18:42:37 -0700

    added the custom network

--- export_onnx.py
+++ export_onnx.py
@@ -1,13 +1,14 @@
 # %%
-from stable_baselines3 import PPO
+from stable_baselines3 import PPO, SAC
 import torch
-loaded_model = PPO.load(r"C:\Users\user\Desktop\RLNav\RLAgency\Results\0_Pipeline\Mutliple\PPO\SB4.zip")
+loaded_model = PPO.load(r"C:\Users\batua\Desktop\RLNav\RLAgency\results\Jump\PPO_Occupancy\Occupancy93_8_LRcon-4_BS1024_GAE0.95_HU256\Jump_92.8%.zip")
+loaded_model = SAC.load(r"C:\Users\batua\Desktop\RLNav\RLAgency\results\JumpV3\Occupancy\BoxSize2_463\JumpV3_99.8%.zip")
 
 
 # %%
-class OnnxablePolicy(torch.nn.Module):
+class PPOOnnxablePolicy(torch.nn.Module):
     def __init__(self, extractor, action_net, value_net):
-        super(OnnxablePolicy, self).__init__()
+        super(PPOOnnxablePolicy, self).__init__()
         self.extractor = extractor
         self.action_net = action_net
         self.value_net = value_net
@@ -16,15 +17,25 @@ class OnnxablePolicy(torch.nn.Module):
         action_hidden, value_hidden = self.extractor(input)
         return (self.action_net(action_hidden), self.value_net(value_hidden))
 
-new_model = OnnxablePolicy(loaded_model.policy.mlp_extractor, loaded_model.policy.action_net, loaded_model.policy.value_net)
-# %%
-new_model(torch.tensor([[0,0,0,0,0,0]], dtype=torch.float32))
+class SACOnnxablePolicy(torch.nn.Module):
+    def __init__(self,  actor):
+        super(SACOnnxablePolicy, self).__init__()
+        # Removing the flatten layer because it can't be onnxed
+        self.actor = torch.nn.Sequential(actor.latent_pi, actor.mu)
 
+    def forward(self, input):
+        return self.actor(input)
 
+if isinstance(loaded_model, PPO):  
+    new_model = PPOOnnxablePolicy(loaded_model.policy.mlp_extractor, loaded_model.policy.action_net, loaded_model.policy.value_net)
+else:
+    new_model = SACOnnxablePolicy(loaded_model.policy.actor)
+
+print(new_model)
 # %%
-dummy_input = torch.randn(1, 6)
+dummy_input = torch.randn(1, 463)
 loaded_model.policy.to("cpu")
-torch.onnx.export(new_model, dummy_input, "navmodel.onnx", opset_version=9)
+torch.onnx.export(new_model, dummy_input, "JumpV3_463_998.onnx", opset_version=9)
 
 
 # %%
